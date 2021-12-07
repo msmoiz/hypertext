d8ec171881a5ea46583ad4ac69e82d989a814899
Mustafa Moiz <mustafa.moiz125@gmail.com>
2021-12-07 15:51:32 -0800

    Add explanatory comments to request fields.

--- hypertext/request.h
+++ hypertext/request.h
@@ -9,6 +9,9 @@
 
 namespace hypertext
 {
+	/**
+	 * Represents an HTTP request.
+	 */
 	class Request
 	{
 	public:
@@ -18,13 +21,53 @@ namespace hypertext
 
 		static class RequestBuilder build();
 
+		/**
+		 * The HTTP method to use.
+		 * Defaults to Method::Get.
+		 */
 		Value<Method> method{Method::Get};
+
+		/**
+		 * The host to send the request to.
+		 * Can be a hostname or IPv4 address.
+		 */
 		Value<std::string> host;
+
+		/**
+		 * Port on the host to send the request to.
+		 * Defaults to 80.
+		 */
 		Value<int> port{80};
+
+		/**
+		 * Resource targeted by the request.
+		 * Defaults to "/" (i.e., root).
+		 */
 		Value<std::string> resource{"/"};
+
+		/**
+		 * Parameters to send with the request.
+		 * Will be sent without manipulation,
+		 * which means users must manually encode these entries.
+		 */
 		Value<std::unordered_map<std::string, std::string>> parameters;
+
+		/**
+		 * Version of HTTP to use for the request.
+		 * Defaults to Version::OnePointOne.
+		 */
 		Value<Version> version{Version::OnePointOne};
+
+		/**
+		 * Headers to send with the request.
+		 * Will be sent without manipulation.
+		 */
 		Value<std::unordered_map<std::string, std::string>> headers;
+
+		/**
+		 * Body to send with the request.
+		 * Will be sent without manipulation.
+		 */
 		Value<std::string> body;
 
 	private:
