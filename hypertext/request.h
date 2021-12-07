860dc54367066a8f8f320eb09ad1bd825ef282b0
Mustafa Moiz <mustafa.moiz125@gmail.com>
2021-12-07 12:09:22 -0800

    Implement builder pattern for request.
    The data required to populate a request can be limited in some cases
    and voluminous in others. The builder pattern is a good solution for
    this type of structure.

--- hypertext/request.h
+++ hypertext/request.h
@@ -1,18 +1,54 @@
 #pragma once
 
 #include <unordered_map>
+#include <utility>
 
 #include "method.h"
 #include "version.h"
+#include "utils/value.h"
 
 namespace hypertext
 {
-	struct Request
+	class Request
 	{
-		Method method;
-		std::string uri;
-		Version version;
-		std::unordered_map<std::string, std::string> headers;
-		std::string body;
+	public:
+
+		template<typename T>
+		using Value = Value<T, class RequestBuilder>;
+
+		static class RequestBuilder build();
+
+		Value<Method> method{Method::Get};
+		Value<std::string> uri;
+		Value<Version> version{Version::OnePointOne};
+		Value<std::unordered_map<std::string, std::string>> headers;
+		Value<std::string> body;
+
+	private:
+
+		Request() = default;
+		friend class RequestBuilder;
 	};
+
+	class RequestBuilder
+	{
+	public:
+
+		RequestBuilder& method(const Method method) { *request_.method = method; return *this; }
+		RequestBuilder& uri(std::string uri) { *request_.uri = std::move(uri); return *this; }
+		RequestBuilder& version(const Version version) { *request_.version = version; return *this; }
+		RequestBuilder& header(std::string key, std::string value) { (*request_.headers)[std::move(key)] = std::move(value); return *this; }
+		RequestBuilder& body(std::string body) { *request_.body = std::move(body); return *this; }
+
+		operator Request() { return std::move(request_); }
+		
+	private:
+
+		Request request_;
+	};
+
+	inline RequestBuilder Request::build()
+	{
+		return RequestBuilder{};
+	}
 }
---
+++ hypertext/utils/value.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <utility>
+
+class NoOne {};
+
+/**
+ * Represents a value that is readonly to the general public.
+ * It can be written to by a single class, specified at compile time,
+ * which makes it a good candidate for use with Builder or other
+ * constructor-style classes.
+ */
+template<typename Type, typename Writer = NoOne>
+class Value
+{
+public:
+
+	Value() = default;
+	explicit Value(Type value): value_(std::move(value)) {}
+	const Type& operator()() const { return value_; }
+	operator const Type&() const { return value_; }
+	
+private:
+
+	Type& operator*() { return value_; }
+	
+	Type value_;
+	friend Writer;
+};
