e7e3df517c64e02635016c4876f27cfb1e8340d3
Mustafa Moiz <mustafa.moiz125@gmail.com>
2021-12-12 18:24:49 -0800

    Cleanup demo program.

--- demo/main.cpp
+++ demo/main.cpp
@@ -8,23 +8,26 @@ int main()
 	using namespace hypertext;
 
 	Request request = Request::build()
-	                  .host("www.google.com")
-	                  .port(80)
-	                  .resource("/");
+	                  .host("jsonplaceholder.typicode.com")
+	                  .header("Host", "jsonplaceholder.typicode.com")
+	                  .resource("/todos/1");
 
 	try
 	{
+		std::cout << "Sending request to " << request.host() << request.resource() << std::endl;
+		
 		Client client;
 		const Response response{client.dispatch(std::move(request))};
+		
 		std::cout
-			<< "API returned with " << std::endl
-			<< "Status code: " << (int)response.status_code() << std::endl
-			<< "Body: " << response.body() << std::endl;
+			<< "Received response " << (int)response.status_code() << " (" << response.status_message() << ")" << std::endl
+			<< "--------------" << std::endl
+			<< response.body() << std::endl;
 	}
 	catch (Exception& e)
 	{
 		std::cerr << "Encountered error while fetching response: " << e.what();
 	}
-	
+
 	return 0;
 }
